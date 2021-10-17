a5a0e33c6432fb4a6ec9eec6f2f385ec12aea400
alexandregeek <alexandregeek@hotmail.com>
2021-10-17 15:44:13 -0300

    busca por id ok

---
+++ src/main/java/com/alexandre_oliveira/crud_client/repositories/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.alexandre_oliveira.crud_client.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexandre_oliveira.crud_client.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository <Client, Long >{
+
+
+}
